84b288acc805b33a630fff660e7d3712cdc71e2d
vaishnavikhosare <vaishnavikhosare31@gmail.com>
2026-03-29 16:03:28 +0530

    added two files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+<<<<<<< HEAD
 
 import ManagerDashboard from "./pages/Manager/ManagerDashboard";
 import EmployeeDashboard from "./pages/employee/EmployeeDashboard";
+=======
+import LandingPage from "./pages/LandingPage.jsx";
+import Login from "./pages/Login.jsx";
+import SignIn from "./pages/SignIn.jsx";
+>>>>>>> 96996f77a7b3d2ff35356d3d54996cfbc08707cc
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+<<<<<<< HEAD
         <Route path="/" element={<ManagerDashboard />} />   {/* ✅ default */}
         <Route path="/manager" element={<ManagerDashboard />} />
         <Route path="/employee" element={<EmployeeDashboard />} />
+=======
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<SignIn />} />
+>>>>>>> 96996f77a7b3d2ff35356d3d54996cfbc08707cc
       </Routes>
     </BrowserRouter>
   );
 }
 
+<<<<<<< HEAD
 export default App;
+=======
+export default App;
+>>>>>>> 96996f77a7b3d2ff35356d3d54996cfbc08707cc
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+<<<<<<< HEAD
 import "./index.css";
+=======
+import "./style/index.css";
+>>>>>>> 96996f77a7b3d2ff35356d3d54996cfbc08707cc
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+<<<<<<< HEAD
   </React.StrictMode>
 );
+=======
+  </React.StrictMode>,
+);
+>>>>>>> 96996f77a7b3d2ff35356d3d54996cfbc08707cc
